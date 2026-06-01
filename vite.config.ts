f87f25be4dc57dc7a2741f9c0a5cbfb64a2272f5
41eks <koukyokutou123@gmail.com>
2026-06-02 00:08:17 +0900

    fix h inject

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,23 @@ export default defineConfig({
     jsxFactory: 'h',
     jsxFragment: 'Fragment',
     // 自动注入 import，这样你就不需要在每个 TSX 文件顶层手动 import h 和 Fragment 了
-    jsxInject: `import { h, Fragment } from '/src/h.ts'`
-  }
+    // jsxInject: `import { h, Fragment } from '/src/h.ts'`
+  }, plugins: [
+    {
+      name: 'auto-inject-jsx-factory',
+      enforce: 'pre',
+      transform(code, id) {
+        // 过滤掉 Vite 可能带有的 query 参数（如 ?vue, ?jsx）
+        const [filename] = id.split('?');
+
+        // 只针对真正的 .tsx 和 .jsx 文件注入，完美的避开了 src/h.ts
+        if (filename && (filename.endsWith('.tsx') || filename.endsWith('.jsx'))) {
+          return {
+            code: `import { h, Fragment } from '/src/h.ts';\n${code}`,
+            map: null // 传 null 让 Vite 自动处理后续的 SourceMap 映射
+          };
+        }
+      }
+    }
+  ]
 })
